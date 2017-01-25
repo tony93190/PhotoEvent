583a6fb554b094028064c3bb645a5b3dfb641eb4
Alexandre34 <alex.morland@orange.fr>
2017-01-25 08:20:14 +0100

    Gros poisson

--- Hello/Hello World/src/Principale.java
+++ Hello/Hello World/src/Principale.java
@@ -2,7 +2,7 @@
 public class Principale {
 
 	public static void main(String[] args) {
-		System.out.println("Hey !");
+		System.out.println("Hey I'm back, it's cool isn't it ?");
 		int a = 3;
 		System.out.println(a+" + 1 = " +(a+1));
 
